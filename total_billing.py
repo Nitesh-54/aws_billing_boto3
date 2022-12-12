182e2f0aff85284590267be9309f28323a475d1c
Nitesh <93045970+Nitesh-54@users.noreply.github.com>
2022-12-12 13:59:31 +0530

    month_wise_bill

--- total_billing.py
+++ total_billing.py
@@ -1,28 +1,20 @@
 import boto3
-from datetime import datetime, timedelta, date
-import time
+from datetime import date
 from dateutil.relativedelta import relativedelta
 
-end_date = datetime.strptime(
-    (datetime.utcnow().strftime('%Y-%m-%d')), '%Y-%m-%d')
-end_date1 = str(end_date)
-end_date1 = end_date1.split(' ')
-end = end_date1[0]
+end_date = date.today()
+end_date = str(end_date)
 
-print(end)
+from_date = date.today().replace(day=1)
 
-start_date = end_date - relativedelta(months=5)
+start_date = from_date - relativedelta(months=3)
 start_date = str(start_date)
-start_date = start_date.split(' ')
-start = start_date[0]
-
-print(start)
 
 client = boto3.client('ce', region_name='us-east-1')
 response = client.get_cost_and_usage(
     TimePeriod={
-        'Start': start,
-        'End': end
+        'Start': start_date,
+        'End': end_date
     },
     Granularity='MONTHLY',
     Metrics=[
@@ -32,5 +24,8 @@ response = client.get_cost_and_usage(
 bill = 0
 for i in range(len(response["ResultsByTime"])):
     int_bill = response["ResultsByTime"][i]["Total"]["AmortizedCost"]["Amount"]
+    print("Bill from ", response["ResultsByTime"][i]["TimePeriod"]["Start"],
+          "-", response["ResultsByTime"][i]["TimePeriod"]["End"], ": ", int_bill)
+
     bill = bill + float(int_bill)
-print(bill)
+print("Total bill : ", bill)
