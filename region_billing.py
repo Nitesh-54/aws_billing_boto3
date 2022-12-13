b77dfa77adcb992c29e50718a200f5959c308cc1
Nitesh <93045970+Nitesh-54@users.noreply.github.com>
2022-12-13 13:16:10 +0530

    Update region_billing.py

--- region_billing.py
+++ region_billing.py
@@ -1,63 +1,40 @@
 import boto3
-
 from datetime import date
-
 from dateutil.relativedelta import relativedelta
 
-
 end_date = date.today()
-
 end_date = str(end_date)
+
+updated_date = date.today().replace(day=1)
+
+start_date = updated_date - relativedelta(months=3)
+start_date = str(start_date)
+
+print(start_date)
 print(end_date)
 
-
-from_date = date.today().replace(day=1)
-
-
-start_date = from_date - relativedelta(months=3)
-
-start_date = str(start_date)
-print(start_date)
-
 client = boto3.client('ce', region_name='us-east-1')
-
 cost_and_usage = client.get_cost_and_usage(
-
     TimePeriod={
-
         "Start": start_date,
-
         "End": end_date
-
     },
-
     Granularity='MONTHLY',
-
     Metrics=['UnblendedCost'],
-
     GroupBy=[
-
         {
-
             "Type": "DIMENSION",
-
             "Key": "REGION"
-
         }
-
     ]
-
 )
-
 
 services = {}
 
 for i in range(len(cost_and_usage["ResultsByTime"])):
-
     for j in range(len(cost_and_usage["ResultsByTime"][i]["Groups"])):
         key = cost_and_usage["ResultsByTime"][i]["Groups"][j]["Keys"][0]
         value = cost_and_usage["ResultsByTime"][i]["Groups"][j]["Metrics"]["UnblendedCost"]["Amount"]
-
         if key not in services:
             services[key] = float(value)
         else:
