49bb8ab1e68278d9a7788589bfbf7b4aa29d8480
Nitesh <93045970+Nitesh-54@users.noreply.github.com>
2022-12-13 13:23:27 +0530

    Update monthly_billing.py

--- monthly_billing.py
+++ monthly_billing.py
@@ -5,10 +5,13 @@ from dateutil.relativedelta import relativedelta
 end_date = date.today()
 end_date = str(end_date)
 
-from_date = date.today().replace(day=1)
+updated_date = date.today().replace(day=1)
 
-start_date = from_date - relativedelta(months=3)
+start_date = updated_date - relativedelta(months=3)
 start_date = str(start_date)
+
+print(start_date)
+print(end_date)
 
 client = boto3.client('ce', region_name='us-east-1')
 response = client.get_cost_and_usage(
@@ -18,14 +21,15 @@ response = client.get_cost_and_usage(
     },
     Granularity='MONTHLY',
     Metrics=[
-        'AmortizedCost',
+        'UnblendedCost',
     ]
 )
+
 bill = 0
 for i in range(len(response["ResultsByTime"])):
-    int_bill = response["ResultsByTime"][i]["Total"]["AmortizedCost"]["Amount"]
+    int_bill = response["ResultsByTime"][i]["Total"]["UnblendedCost"]["Amount"]
     print("Bill from ", response["ResultsByTime"][i]["TimePeriod"]["Start"],
-          "-", response["ResultsByTime"][i]["TimePeriod"]["End"], ": ", int_bill)
-
+          "-", response["ResultsByTime"][i]["TimePeriod"]["End"], ":", int_bill)
     bill = bill + float(int_bill)
-print("Total bill : ", bill)
+
+print("Total bill:", bill)
