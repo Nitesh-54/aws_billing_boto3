77d27fd4e841af205ba8bd7b693c5ef0bd0a4f19
Nitesh <93045970+Nitesh-54@users.noreply.github.com>
2022-12-13 13:05:53 +0530

    Update account_billing.py

--- account_billing.py
+++ account_billing.py
@@ -1,58 +1,36 @@
 import boto3
-
 from datetime import date
-
 from dateutil.relativedelta import relativedelta
 
 
 
 end_date = date.today()
-
 end_date = str(end_date)
 print(end_date)
 
-
-
 from_date = date.today().replace(day=1)
 
-
 start_date = from_date - relativedelta(months=3)
-
 start_date = str(start_date)
 print(start_date)
 
 client = boto3.client('ce', region_name='us-east-1')
-
 sample = client.get_cost_and_usage(
-
     TimePeriod={
-
         "Start": start_date,
-
         "End": end_date
-
     },
-
     Granularity='MONTHLY',
-
     Metrics=['UnblendedCost'],
-
     GroupBy=[
-
         {
-
             "Type": "DIMENSION",
-
             "Key": "LINKED_ACCOUNT"
-
         }
-
     ]
-
 )
 
 accounts = {}
-
 for i in range(len(sample["ResultsByTime"])):
     for j in range(len(sample["ResultsByTime"][i]["Groups"])):
         key = sample["ResultsByTime"][i]["Groups"][j]["Keys"][0]
